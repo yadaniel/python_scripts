138123375911b7ecf71b36f2fa8168580d95842d
yadaniel <ya.daniel@inbox.ru>
2019-01-14 22:32:19 +0100

    initial

---
+++ sendtex/get_stocks.py
@@ -0,0 +1,11 @@
+#!/cygdrive/c/Python36/python
+
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+from matplotlib.finance import candlestick_ohlc
+import matplotlib.dates as mdates
+import pandas as pd
+import pandas_datareader.data as web
+
+
