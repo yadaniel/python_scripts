7e315a4c96d8f3516c3d7c26e2a77a4c4f722ba2
yadaniel <ya.daniel@inbox.ru>
2019-04-28 16:19:31 +0200

    upgrade corrected, mp3 convert option added

--- youtube_dl/you.sh
+++ youtube_dl/you.sh
@@ -3,6 +3,16 @@
 PYTHON_PATH=/cygdrive/c/Python37/python
 PIP=/cygdrive/c/Python37/Scripts/pip
 
+function usage() {
+    echo "you.sh <name> <url> [mp3]"
+    echo "    <name> = name to save"
+    echo "    <url> = url"
+    echo "    [mp3] = optional convert to mp3"
+    exit
+}
+
+[ $1 ] && [ $2 ] || usage
+
 case $1 in
     version) 
         echo version
@@ -10,11 +20,12 @@ case $1 in
         ;;
     update) 
         echo update
-        env PATH=$PYTHON_PATH:/bin /cygdrive/c/Python37/Scripts/pip install youtube-dl
+        env PATH=$PYTHON_PATH:/bin /cygdrive/c/Python37/Scripts/pip install --upgrade youtube-dl
         ;;
     *)
         echo downloading
         env PATH=$PYTHON_PATH:/bin /cygdrive/c/Python37/Scripts/youtube-dl --no-check-certificate -o $1 $2
+        [ $3 ] && [ $3 == mp3 ] && ffmpeg -i $1 -vn -ar 44100 -ac 2 -b:a 192k $1.mp3 # && cp $1.mp3 /cygdrive/g
         ;;
 esac
 
